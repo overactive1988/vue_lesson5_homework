800aed51d3a029b3a12545268c8354c1f647fcf2
bhw-lee <t.lee@bhworks.com>
2021-06-07 14:32:57 +0900

    將原本定義的 接近全域變數 改寫為 ref型式 來使用

---
+++ htdocs/assets/js/admin/adminProductModal.js
@@ -0,0 +1,289 @@
+export default {
+  template: `<div
+  class="modal fade"
+  ref="productModal"
+  tabindex="-1"
+  aria-labelledby="productModalLabel"
+  aria-hidden="true"
+>
+  <div class="modal-dialog modal-xl modal-dialog-centered">
+    <div class="modal-content border-0">
+      <div class="modal-header bg-success text-white">
+        <h5 id="productModalLabel" class="modal-title">
+          <span>新增產品</span>
+        </h5>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="mb-1">
+              <div class="form-group">
+                <label class="text-light bg-secondary mb-2 py-1 px-2" for="mainImageUrl">請輸入主圖片網址</label>
+                <input
+                  id="mainImageUrl"
+                  type="text"
+                  class="form-control"
+                  placeholder="請輸入圖片連結"
+                  v-model.trim="whereProduct.imageUrl"
+                />
+                <img
+                  class="img-fluid my-3"
+                  :src="whereProduct.imageUrl"
+                  :alt="whereProduct.title"
+                />
+              </div>
+
+              <div>
+              <label class="btn btn-outline-success btn-sm d-block w-100 mt-3">
+                <input id="upload_img" style="display:none;" type="file" @change="uploadMainImgage">
+                上傳圖片
+              </label>
+              </div>
+
+            </div>
+            <div class="mt-4 mb-1">多圖新增</div>
+            <!-- 大寫開頭 建構函式 -->
+            <div v-if="Array.isArray(whereProduct.imagesUrl)">
+              <div
+                class="mb-4"
+                v-for="(item,index) in whereProduct.imagesUrl"
+                :key="'addImage'+index"
+              >
+                <div class="form-group">
+                  <label class="text-light bg-secondary mb-2 py-1 px-2" for="subImageUrl">請輸入副圖片網址</label>
+                  <input
+                    id="subImageUrl"
+                    type="text"
+                    class="form-control"
+                    placeholder="請輸入圖片連結"
+                    v-model="whereProduct.imagesUrl[index]"
+                  />
+                  <img class="img-fluid my-3" :src="item" alt />
+
+                    <div>
+                    <label class="btn btn-outline-success btn-sm d-block w-100 my-3">
+                      <input id="upload_img" style="display:none;" type="file" @change="uploadSubImgage">
+                      上傳圖片
+                    </label>
+                    </div>
+
+                  <div class="mb2">
+                    <button
+                      class="btn btn-outline-danger btn-sm d-block w-100"
+                      @click="whereProduct.imagesUrl.splice(index,1)"
+                    >
+                      刪除圖片
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="mt-4"
+                v-if="!whereProduct.imagesUrl.length || whereProduct.imagesUrl[whereProduct.imagesUrl.length-1]"
+              >
+                <button
+                  class="btn btn-outline-primary btn-sm d-block w-100"
+                  @click="whereProduct.imagesUrl.push('')"
+                >
+                  新增圖片
+                </button>
+              </div>
+            </div>
+            <div v-else>
+              <div class="mt-4 mb-2">
+                <button
+                  class="btn btn-outline-primary btn-sm d-block w-100"
+                  @click="createImages"
+                >
+                  新增圖片
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-8">
+            <div class="form-group">
+              <label for="title">標題</label>
+              <input
+                id="title"
+                type="text"
+                class="form-control"
+                placeholder="請輸入標題"
+                v-model.trim="whereProduct.title"
+              />
+            </div>
+
+            <div class="row">
+              <div class="form-group col-md-6">
+                <label for="category">分類</label>
+                <input
+                  id="category"
+                  type="text"
+                  class="form-control"
+                  placeholder="請輸入分類"
+                  v-model="whereProduct.category"
+                />
+              </div>
+              <div class="form-group col-md-6">
+                <label for="unit">單位(個、台、隻...)</label>
+                <input
+                  id="unit"
+                  type="text"
+                  class="form-control"
+                  placeholder="請輸入單位"
+                  v-model="whereProduct.unit"
+                />
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="form-group col-md-6">
+                <label for="origin_price">原價</label>
+                <input
+                  id="origin_price"
+                  type="number"
+                  min="0"
+                  class="form-control"
+                  placeholder="請輸入原價"
+                  v-model.number="whereProduct.origin_price"
+                />
+              </div>
+              <div class="form-group col-md-6">
+                <label for="price">售價</label>
+                <input
+                  id="price"
+                  type="number"
+                  min="0"
+                  class="form-control"
+                  placeholder="請輸入售價"
+                  v-model.number="whereProduct.price"
+                />
+              </div>
+            </div>
+            <hr />
+
+            <div class="form-group">
+              <label for="description">產品描述</label>
+              <textarea
+                id="description"
+                type="text"
+                class="form-control"
+                placeholder="請輸入產品描述"
+                v-model.trim="whereProduct.description"
+              >
+              </textarea>
+            </div>
+            <div class="form-group">
+              <label for="content">說明內容</label>
+              <textarea
+                id="content"
+                type="text"
+                class="form-control"
+                placeholder="請輸入說明內容"
+                v-model.trim="whereProduct.content"
+              >
+              </textarea>
+            </div>
+            <div class="form-group">
+              <div class="form-check">
+                <input
+                  id="is_enabled"
+                  class="form-check-input"
+                  type="checkbox"
+                  :true-value="1"
+                  :false-value="0"
+                  v-model="whereProduct.is_enabled"
+                />
+                <label class="form-check-label" for="is_enabled"
+                  >是否啟用</label
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button
+          type="button"
+          class="btn btn-outline-secondary"
+          data-bs-dismiss="modal"
+        >
+          取消
+        </button>
+        <button
+          type="button"
+          class="btn btn-primary"
+          @click="$emit('update-product',whereProduct)"
+        >
+          確認
+        </button>
+      </div>
+    </div>
+  </div>
+  </div>`,
+  props: {
+    whereProduct: {
+      type: Object, // 驗證型別
+      default(){
+        return {
+        }
+      }
+    },
+  },
+  methods: {
+    // 新增陣列圖片
+    createImages() {
+      this.whereProduct.imagesUrl = ['']
+    },
+    uploadMainImgage(e) {
+      console.dir(e);
+      const url = `${baseUrl}/api/${apiPath}/admin/upload`
+      const file = e.target.files[0];
+      const formData = new FormData();
+      formData.append("file-to-upload", file);
+      axios
+        .post(url, formData)
+        .then((res) => {
+          console.log(res);
+          this.whereProduct.imageUrl = res.data.imageUrl;
+        }).catch((err=>{
+          console.log(err);
+        }));
+    },
+    uploadSubImgage(e) {
+      console.dir(e);
+      const url = `${baseUrl}/api/${apiPath}/admin/upload`
+      const file = e.target.files[0];
+      const formData = new FormData();
+      formData.append("file-to-upload", file);
+      axios
+        .post(url, formData)
+        .then((res) => {
+          console.log(res);
+          this.whereProduct.imagesUrl[this.whereProduct.imagesUrl.length-1]= res.data.imageUrl;
+        }).catch((err=>{
+          console.log(err);
+        }));
+    },
+    openAdminModal(){
+      this.modal.show();
+    },
+    closeAdminModal(){
+      this.modal.hide();
+    },
+  },
+  data(){
+    return {
+      modal: '',
+    }
+  },
+  mounted(){
+    // Bootstrap Modal實體掛載
+    this.modal = new bootstrap.Modal(this.$refs.productModal);
+  }
+}
--- htdocs/assets/js/vue_products.js
+++ htdocs/assets/js/vue_products.js
@@ -1,8 +1,6 @@
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.11/vue.esm-browser.js'
 import pagination from './pagination.js'
-
-let productModal = null // 定義接近全域變數
-let delProductModal = null // 定義接近全域變數
+import adminProducts from './admin/adminProductModal.js'
 
 const app = createApp({
   data() {
@@ -33,12 +31,6 @@ const app = createApp({
     )
     // 將 Token 夾帶至請請求(request) 的表頭(headers) 設定只需要一次
     axios.defaults.headers.common['Authorization'] = token
-
-    // Bootstrap Modal實體掛載
-    productModal = new bootstrap.Modal(document.getElementById('productModal'))
-    delProductModal = new bootstrap.Modal(
-      document.getElementById('delProductModal')
-    )
 
     this.loginCheck()
     this.getData()
@@ -89,7 +81,7 @@ const app = createApp({
       axios
         .delete(url)
         .then((res) => {
-          delProductModal.hide() // 關掉 modal
+          this.$refs.adminDelModal.closeAdminModal() // 關掉 modal
           alert(`已刪除${this.tempProduct.title}商品`)
           this.getData()
         })
@@ -107,20 +99,20 @@ const app = createApp({
             imagesUrl: [],
           }
           this.isNew = true
-          productModal.show()
+          this.$refs.adminControlModal.openAdminModal()
           break
         case 'edit':
           // 因為傳參考特性會連動到資料，因此將資料進行淺層複製
           this.getData()
           this.tempProduct = { ...item}
           this.isNew = false
-          productModal.show()
+          this.$refs.adminControlModal.openAdminModal()
           break
         case 'delete':
           // 因為傳參考特性會連動到資料，因此將資料進行淺層複製
           this.tempProduct = { ...item}
           // Modal需要拿到 title 和刪除按鈕時需要獲得 id
-          delProductModal.show()
+          this.$refs.adminDelModal.openAdminModal()
           break
         default:
           break
@@ -140,7 +132,7 @@ const app = createApp({
       axios[httpMethod](url, { data: tempProduct }) // post 或 put
         .then((res) => {
           if (res.data.success) {
-            productModal.hide() // 關掉 modal
+            this.$refs.productModal.hide() // 關掉 modal
             this.getData() // 重整畫面
           } else {
             alert(res.data.message)
@@ -165,281 +157,7 @@ const app = createApp({
 
 // 定義全域元件 須放置在 createApp 後方，mount之前
 // 商品 Modal
-app.component('productModal',{
-  props: {
-    whereProduct: {
-      type: Object, // 驗證型別
-      default(){
-        return {
-        }
-      }
-    },
-  },
-  template: `<div
-  id="productModal"
-  ref="productModal"
-  class="modal fade"
-  tabindex="-1"
-  aria-labelledby="productModalLabel"
-  aria-hidden="true"
->
-  <div class="modal-dialog modal-xl modal-dialog-centered">
-    <div class="modal-content border-0">
-      <div class="modal-header bg-success text-white">
-        <h5 id="productModalLabel" class="modal-title">
-          <span>新增產品</span>
-        </h5>
-        <button
-          type="button"
-          class="btn-close"
-          data-bs-dismiss="modal"
-          aria-label="Close"
-        ></button>
-      </div>
-      <div class="modal-body">
-        <div class="row">
-          <div class="col-sm-4">
-            <div class="mb-1">
-              <div class="form-group">
-                <label class="text-light bg-secondary mb-2 py-1 px-2" for="mainImageUrl">請輸入主圖片網址</label>
-                <input
-                  id="mainImageUrl"
-                  type="text"
-                  class="form-control"
-                  placeholder="請輸入圖片連結"
-                  v-model.trim="whereProduct.imageUrl"
-                />
-                <img
-                  class="img-fluid my-3"
-                  :src="whereProduct.imageUrl"
-                  :alt="whereProduct.title"
-                />
-              </div>
-
-              <div>
-              <label class="btn btn-outline-success btn-sm d-block w-100 mt-3">
-                <input id="upload_img" style="display:none;" type="file" @change="uploadMainImgage">
-                上傳圖片
-              </label>
-              </div>
-
-            </div>
-            <div class="mt-4 mb-1">多圖新增</div>
-            <!-- 大寫開頭 建構函式 -->
-            <div v-if="Array.isArray(whereProduct.imagesUrl)">
-              <div
-                class="mb-4"
-                v-for="(item,index) in whereProduct.imagesUrl"
-                :key="'addImage'+index"
-              >
-                <div class="form-group">
-                  <label class="text-light bg-secondary mb-2 py-1 px-2" for="subImageUrl">請輸入副圖片網址</label>
-                  <input
-                    id="subImageUrl"
-                    type="text"
-                    class="form-control"
-                    placeholder="請輸入圖片連結"
-                    v-model="whereProduct.imagesUrl[index]"
-                  />
-                  <img class="img-fluid my-3" :src="item" alt />
-
-                    <div>
-                    <label class="btn btn-outline-success btn-sm d-block w-100 my-3">
-                      <input id="upload_img" style="display:none;" type="file" @change="uploadSubImgage">
-                      上傳圖片
-                    </label>
-                    </div>
-
-                  <div class="mb2">
-                    <button
-                      class="btn btn-outline-danger btn-sm d-block w-100"
-                      @click="whereProduct.imagesUrl.splice(index,1)"
-                    >
-                      刪除圖片
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div
-                class="mt-4"
-                v-if="!whereProduct.imagesUrl.length || whereProduct.imagesUrl[whereProduct.imagesUrl.length-1]"
-              >
-                <button
-                  class="btn btn-outline-primary btn-sm d-block w-100"
-                  @click="whereProduct.imagesUrl.push('')"
-                >
-                  新增圖片
-                </button>
-              </div>
-            </div>
-            <div v-else>
-              <div class="mt-4 mb-2">
-                <button
-                  class="btn btn-outline-primary btn-sm d-block w-100"
-                  @click="createImages"
-                >
-                  新增圖片
-                </button>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-8">
-            <div class="form-group">
-              <label for="title">標題</label>
-              <input
-                id="title"
-                type="text"
-                class="form-control"
-                placeholder="請輸入標題"
-                v-model.trim="whereProduct.title"
-              />
-            </div>
-
-            <div class="row">
-              <div class="form-group col-md-6">
-                <label for="category">分類</label>
-                <input
-                  id="category"
-                  type="text"
-                  class="form-control"
-                  placeholder="請輸入分類"
-                  v-model="whereProduct.category"
-                />
-              </div>
-              <div class="form-group col-md-6">
-                <label for="unit">單位(個、台、隻...)</label>
-                <input
-                  id="unit"
-                  type="text"
-                  class="form-control"
-                  placeholder="請輸入單位"
-                  v-model="whereProduct.unit"
-                />
-              </div>
-            </div>
-
-            <div class="row">
-              <div class="form-group col-md-6">
-                <label for="origin_price">原價</label>
-                <input
-                  id="origin_price"
-                  type="number"
-                  min="0"
-                  class="form-control"
-                  placeholder="請輸入原價"
-                  v-model.number="whereProduct.origin_price"
-                />
-              </div>
-              <div class="form-group col-md-6">
-                <label for="price">售價</label>
-                <input
-                  id="price"
-                  type="number"
-                  min="0"
-                  class="form-control"
-                  placeholder="請輸入售價"
-                  v-model.number="whereProduct.price"
-                />
-              </div>
-            </div>
-            <hr />
-
-            <div class="form-group">
-              <label for="description">產品描述</label>
-              <textarea
-                id="description"
-                type="text"
-                class="form-control"
-                placeholder="請輸入產品描述"
-                v-model.trim="whereProduct.description"
-              >
-              </textarea>
-            </div>
-            <div class="form-group">
-              <label for="content">說明內容</label>
-              <textarea
-                id="content"
-                type="text"
-                class="form-control"
-                placeholder="請輸入說明內容"
-                v-model.trim="whereProduct.content"
-              >
-              </textarea>
-            </div>
-            <div class="form-group">
-              <div class="form-check">
-                <input
-                  id="is_enabled"
-                  class="form-check-input"
-                  type="checkbox"
-                  :true-value="1"
-                  :false-value="0"
-                  v-model="whereProduct.is_enabled"
-                />
-                <label class="form-check-label" for="is_enabled"
-                  >是否啟用</label
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button
-          type="button"
-          class="btn btn-outline-secondary"
-          data-bs-dismiss="modal"
-        >
-          取消
-        </button>
-        <button
-          type="button"
-          class="btn btn-primary"
-          @click="$emit('update-product',whereProduct)"
-        >
-          確認
-        </button>
-      </div>
-    </div>
-  </div>
-  </div>`,
-  methods: {
-    // 新增陣列圖片
-    createImages() {
-      this.whereProduct.imagesUrl = ['']
-    },
-    uploadMainImgage(e) {
-      console.dir(e);
-      const url = `${baseUrl}/api/${apiPath}/admin/upload`
-      const file = e.target.files[0];
-      const formData = new FormData();
-      formData.append("file-to-upload", file);
-      axios
-        .post(url, formData)
-        .then((res) => {
-          console.log(res);
-          this.whereProduct.imageUrl = res.data.imageUrl;
-        }).catch((err=>{
-          console.log(err);
-        }));
-    },
-    uploadSubImgage(e) {
-      console.dir(e);
-      const url = `${baseUrl}/api/${apiPath}/admin/upload`
-      const file = e.target.files[0];
-      const formData = new FormData();
-      formData.append("file-to-upload", file);
-      axios
-        .post(url, formData)
-        .then((res) => {
-          console.log(res);
-          this.whereProduct.imagesUrl[this.whereProduct.imagesUrl.length-1]= res.data.imageUrl;
-        }).catch((err=>{
-          console.log(err);
-        }));
-    },
-  }
-})
+app.component('adminProductModal',adminProducts)
 
 // 將 deleteModal x-template 元件化
 app.component('delProductModal',{
@@ -453,5 +171,22 @@ app.component('delProductModal',{
     },
   },
   template: "#delModal-template",
+  data() {
+    return {
+      modal: '',
+    }
+  },
+  mounted(){
+    // Bootstrap Modal實體掛載
+    this.modal = new bootstrap.Modal(this.$refs.delProductModal);
+  },
+  methods:{
+    openAdminModal(){
+      this.modal.show();
+    },
+    closeAdminModal(){
+      this.modal.hide();
+    },
+  },
 })
 app.mount('#app')
